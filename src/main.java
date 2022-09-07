71cc78027bf10fe05de880e4601c6685da309fe6
Ivan Lapin <jeti.vano.l@mail.ru>
2022-09-07 18:26:53 +0300

    initial commit

---
+++ src/main.java
@@ -0,0 +1,17 @@
+public class main {
+    public static void main(String[] args) {
+        int remainder = 200;
+
+        int replenishment = 1100;
+
+        int bonus = replenishment / 100;
+
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
+        }
+
+        System.out.println("Итоговый бонус " + bonus);
+    }
+}
